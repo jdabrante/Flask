f58be65979f65470b1dc0e2481fbd90c4ac92915
Juan Dimas Abrante Hernandez <19jdah98@gmail.com>
2023-06-19 20:46:55 +0100

    Se sube el tutorial de Flask hasta Blueprints and Views

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -1,15 +1,16 @@
 # Este fichero.py sirve para alojar la factoria de aplicación así como para que Python reconozca la carpeta como un paquete
 import os
+from . import db
 from flask import Flask
 
 def create_app(test_config = None):
     # Crea y configura la app
     app = Flask(__name__, instance_relative_config = True)
-    app.config.from_mapping(SECRET_KEY = 'dev', DATABASE = os.path.join(app.instance_path, 'flask.sqlite'),)
+    app.config.from_mapping(SECRET_KEY = 'dev', DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'))
 
     if test_config is None:
         # Load the instace config, if it exists, when not testing
-        app.config.from_file('config.py', silent = True)
+        app.config.from_pyfile('config.py', silent = True)
     else: 
         app.config.from_mapping(test_config)
     
@@ -20,7 +21,10 @@ def create_app(test_config = None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route('/hello/')
     def hello():
         return 'Hello, World!'
+    
+    db.init_app(app)
+    
     return app
